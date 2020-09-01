572f2284585a793fdd7c66bcc267fc0db4cc9c52
M.Talha Javed <talha.tj98@gmail.com>
2020-09-01 23:25:43 +0500

    'initial'

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
+const path = require('path');
+// const cors = require('cors');
+const app = express();
 dotenv.config({
-    path: './config/config.env'
-})
+    path: './config/config.env',
+});
+// // Cors
+// const corsOptions = {
+//   origin: process.env.ALLOWED_CLIENTS.split(','),
+// };
+// app.use(cors(corsOptions));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('FIleSharing app')
-})
+connectDB();
 
+app.use(express.json());
 
-const PORT = process.env.PORT || 3300;
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`)
-})
+    console.log(`Server listening on PORT: ${PORT}`);
+});
